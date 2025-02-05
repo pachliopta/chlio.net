897afc8b144a4f03d8a2fe0016cdd486e9522176
pachliopta <111627835+pachliopta@users.noreply.github.com>
2025-02-05 17:24:21 +1100

    compute status permalinks

--- src/posts/status/status.11tydata.js
+++ src/posts/status/status.11tydata.js
@@ -1,4 +1,10 @@
 export default {
   layout: "layouts/log.njk",
   tags: "status",
+  eleventyComputed: {
+    permalink: function (data) {
+      var date = Math.floor(data.date.getTime() / 1000);
+      return `/status/${date}/index.html`;
+    },
+  },
 };
